8a19d0e78d4b523c4f469bbe691ab46c90c0d98e
Taylor <taylor@acker.co.nz>
2025-04-03 11:47:49 +1300

    laptop and pc differences

--- load.R
+++ load.R
@@ -2,9 +2,9 @@ library(tidyverse)
 library(rdrop2)
 library(lavaan)
 
-drop_auth(new_user = TRUE)
+#drop_auth(new_user = TRUE)
 
-raw_df <- drop_read_csv("Longitudinal Colleges Data - Otago/Belonging_Dataset_Deidentified.csv") %>%
+raw_df <- drop_read_csv("Winter Data/Longitudinal Colleges Data - Otago/Belonging_Dataset_Deidentified.csv") %>%
   as_tibble() %>%
   rename_all(tolower)
 
