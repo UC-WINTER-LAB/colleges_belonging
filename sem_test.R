7b26dd2b8adcff1b8056686a0cc33a8c7ffbabf2
Taylor <taylor.winter@canterbury.ac.nz>
2025-04-03 09:47:36 +1300

    Sem testing

--- sem_test.R
+++ sem_test.R
@@ -16,5 +16,22 @@ sem_data <- new_df_latent_vars %>%
     by="id"
   )
 
+test_sem <- sem(
+  "
+  wmws ~ b1*exits5 + b2*exits8 + c*exits
+  exits5 ~ a1*exits
+  exits8 ~ a2*exits
   
+  m1 := a1*b1
+  m2 := a2*b2
+  
+  total := a1+b1+a2+b2+c
+  
+  m1_prop := m1/total
+  m2_prop := m2/total
+  ",
+  data=sem_data
+)
+
+summary(test_sem)  
   
